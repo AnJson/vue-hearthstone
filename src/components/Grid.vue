ce67494d4cff40f230436c94941f2722c3bcb0d3
Anders Jonsson <aj224rj@student.lnu.se>
2023-04-28 13:49:17 +0200

    add font and configuration to tailwind and a component for testing

--- src/components/Grid.vue
+++ src/components/Grid.vue
@@ -3,5 +3,23 @@
 </script>
 
 <template>
-
+  <div class="flex flex-wrap gap-3">
+    <div class="w-12 h-12 bg-slate-600"></div>
+    <div class="w-12 h-12 bg-slate-600"></div>
+    <div class="w-12 h-12 bg-slate-600"></div>
+    <div class="w-12 h-12 bg-slate-600"></div>
+    <div class="w-12 h-12 bg-slate-600"></div>
+    <div class="w-12 h-12 bg-slate-600"></div>
+    <div class="w-12 h-12 bg-slate-600"></div>
+    <div class="w-12 h-12 bg-slate-600"></div>
+    <div class="w-12 h-12 bg-slate-600"></div>
+    <div class="w-12 h-12 bg-slate-600"></div>
+    <div class="w-12 h-12 bg-slate-600"></div>
+    <div class="w-12 h-12 bg-slate-600"></div>
+    <div class="w-12 h-12 bg-slate-600"></div>
+    <div class="w-12 h-12 bg-slate-600"></div>
+    <div class="w-12 h-12 bg-slate-600"></div>
+    <div class="w-12 h-12 bg-slate-600"></div>
+    <div class="w-12 h-12 bg-slate-600"></div>
+  </div>
 </template>
