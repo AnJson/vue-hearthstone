5194654fa0c55c193877f57e741b6000e38714da
Anders Jonsson <aj224rj@student.lnu.se>
2023-04-30 10:54:08 +0200

    add first color to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
     extend: {
       fontFamily: {
         sans: ['Open Sans', ...defaultTheme.fontFamily.sans]
+      },
+      colors: {
+        'hs-green-1': '#1aa572'
       }
     },
   },
