feb59b5ef77281805af2995e200f3fcc3a3f4f33
Anders Jonsson <aj224rj@student.lnu.se>
2023-05-02 23:03:17 +0200

    add style to card

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,11 +1,78 @@
 <template>
-
+  <div id="card" @click="toggleFlipped">
+    <div :class="frontClasses"></div>
+    <div :class="backClasses"></div>
+  </div>
 </template>
 
 <script setup>
+import { ref, computed } from 'vue'
+
 defineProps({
-  flipped: Boolean,
   front: String,
   back: String
 })
+
+const flipped = ref(false)
+
+const frontClasses = computed(() => {
+  return {
+    ['card-side']: true,
+    ['card-side--front']: true,
+    ['bg-hs-green-1']: true,
+    ['hide-front']: flipped.value
+  }
+})
+
+const backClasses = computed(() => {
+  return {
+    ['card-side']: true,
+    ['card-side--back']: true,
+    ['reveal-back']: flipped.value
+  }
+})
+
+const toggleFlipped = () => {
+  flipped.value = !flipped.value
+}
 </script>
+
+<style scoped>
+#card {
+  position: relative;
+  height: 208px;
+  width: 160px;
+  cursor: pointer;
+  perspective: 50rem;
+  -moz-perspective: 50rem;
+}
+
+.card-side {
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 100%;
+  width: 100%;
+  transition: all 1s ease;
+  backface-visibility: hidden;
+  border-radius: 3px;
+  box-shadow: 0 4px 4px rgba(0, 0, 0, .3);
+  transform-origin: 50% 50%;
+}
+
+.card-side--back {
+  transform: rotateY(180deg);
+}
+
+.card-side--front {
+  transform: rotateY(0)
+}
+
+.reveal-back {
+  transform: rotateY(0);
+}
+
+.hide-front {
+  transform: rotateY(-180deg);
+}
+</style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 const defaultTheme = require('tailwindcss/defaultTheme')
+const plugin = require('tailwindcss/plugin')
+
+const backfaceVisibility = plugin(function({addUtilities}) {
+  addUtilities({
+    '.backface-visible': {
+      'backface-visibility': 'visible',
+    },
+    '.backface-hidden': {
+      'backface-visibility': 'hidden',
+    }
+  })
+})
 
 module.exports = {
   content: ['./index.html', './src/**/*.{vue, js, ts}'],
@@ -13,6 +25,8 @@ module.exports = {
       }
     },
   },
-  plugins: [],
+  plugins: [
+    backfaceVisibility
+  ],
 }
 
