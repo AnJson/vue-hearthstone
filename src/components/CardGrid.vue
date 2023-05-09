3110675f75f5553141f353ba1f6937ffbe202f45
Anders Jonsson <aj224rj@student.lnu.se>
2023-05-09 20:29:08 +0200

    refactor from CardsView to CardGrid

--- src/components/CardGrid.vue
+++ src/components/CardGrid.vue
@@ -1,15 +1,53 @@
 <template>
-  <GridLayout>
-    <Card v-for="card in props.cards" :key="card.cardId" :front="card.img" back="https://youre.outof.games/media/uploads/6c/94/6c945403-1207-454b-a953-a1787ba9016f/cardback_0.png" />
-  </GridLayout>
+  <div class="flex flex-col gap-3 w-full">
+    <h2 class="text-lg">{{ indexOfLastCurrentCard }} / {{ totalResult }}</h2>
+    <GridLayout>
+      <Card v-for="card in currentCards" :key="card.cardId" :front="card.img" back="https://youre.outof.games/media/uploads/6c/94/6c945403-1207-454b-a953-a1787ba9016f/cardback_0.png" />
+    </GridLayout>
+    <div class="flex justify-between mt-auto pb-5 px-3">
+      <h2 class="text-lg">Page {{ props.currentPage }}</h2>
+      <div class="flex gap-3">
+        <RouterLink v-if="previousPage" role="link" class="text-lg" :to="{ name: 'cards', query: { page: props.currentPage - 1 } }">Previous</RouterLink>
+        <RouterLink v-if="nextPage" role="link" class="text-lg" :to="{ name: 'cards', query: { page: props.currentPage + 1 } }">Next</RouterLink>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup>
+import { computed } from 'vue'
 import GridLayout from '@/components/Utils/GridLayout.vue'
 import Card from '@/components/Card.vue'
 
 const props = defineProps({
-  cards: Array
+  cards: Array,
+  cardsToShow: Number,
+  currentPage: Number
 })
 
+const currentCards = computed(() => {
+  const startIndex = ((props.currentPage - 1) * props.cardsToShow)
+  return [...props.cards].splice(startIndex, props.cardsToShow) 
+})
+
+const previousPage = computed(() => {
+  const previousPage = props.currentPage - 1
+  const firstPage = 1
+  return previousPage >= firstPage ? previousPage : undefined
+})
+
+const nextPage = computed(() => {
+  const nextPage = props.currentPage + 1
+  const maxPage = Math.ceil(props.cards.length / props.cardsToShow)
+  return nextPage <= maxPage ? nextPage : undefined
+})
+
+const totalResult = computed(() => {
+  return props.cards.length
+})
+
+const indexOfLastCurrentCard = computed(() => {
+  const index = props.currentPage * props.cardsToShow
+  return index < totalResult.value ? index : totalResult.value
+})
 </script>
--- src/views/CardsView.vue
+++ src/views/CardsView.vue
@@ -9,24 +9,14 @@
           :class="`py-2 px-1 border-b cursor-pointer hover:border-hs-green-1 ${selectedClass === className ? 'active' : '' }`">{{ className }}</li>
       </ul>
     </div>
-    <div class="flex flex-1">
+    <div class="flex flex-1 w-full">
       <h1 v-if="hasError" class="text-lg text-hs-green-1 font-semibold">
         Ooops, something went wrong!
       </h1>
       <h1 v-else-if="isLoading" class="text-lg text-hs-green-1 font-semibold">
         Loading...
       </h1>
-      <div v-else class="flex flex-col gap-3">
-        <h2 class="text-lg">{{ indexOfLastCurrentCard }} / {{ totalResult }}</h2>
-        <CardGrid :cards="currentCards" />
-        <div v-if="data.length" class="flex justify-between mt-auto pb-5 px-3">
-          <h2 class="text-lg">Page {{ currentPage }}</h2>
-          <div class="flex gap-3">
-            <RouterLink v-if="previousPage" role="link" class="text-lg" :to="{ name: 'cards', query: { page: currentPage - 1 } }">Previous</RouterLink>
-            <RouterLink v-if="nextPage" role="link" class="text-lg" :to="{ name: 'cards', query: { page: currentPage + 1 } }">Next</RouterLink>
-          </div>
-        </div>
-      </div>
+      <CardGrid v-else :currentPage="currentPage" :cardsToShow="8" :cards="data" />
     </div>
   </main>
 </template>
@@ -59,35 +49,8 @@ const selectedClass = ref(null)
 const data = ref([])
 const isLoading = ref(false)
 const hasError = ref(false)
-const cardsToShow = 8
 const currentPage = computed(() => {
   return Number.parseInt(route.query.page || 1) 
-})
-
-const currentCards = computed(() => {
-  const startIndex = ((currentPage.value - 1) * cardsToShow)
-  return [...data.value].splice(startIndex, cardsToShow) 
-})
-
-const previousPage = computed(() => {
-  const previousPage = currentPage.value - 1
-  const firstPage = 1
-  return previousPage >= firstPage ? previousPage : undefined
-})
-
-const nextPage = computed(() => {
-  const nextPage = currentPage.value + 1
-  const maxPage = Math.ceil(data.value.length / cardsToShow)
-  return nextPage <= maxPage ? nextPage : undefined
-})
-
-const totalResult = computed(() => {
-  return data.value.length
-})
-
-const indexOfLastCurrentCard = computed(() => {
-  const index = currentPage.value * cardsToShow
-  return index < totalResult.value ? index : totalResult.value
 })
 
 const setSelectedClass = (className) => {
